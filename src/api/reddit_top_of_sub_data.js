8aa75543ab7ff12afadd731cb488fc0476cc3290
Moritz Albach <moritz.albach@gal-digital.de>
2021-06-24 12:51:02 +0200

    get data cant use it

--- src/api/reddit_top_of_sub_data.js
+++ src/api/reddit_top_of_sub_data.js
@@ -1,5 +1,30 @@
-function reddit_top_of_sub_data(sub, limet, time_filter){
+
+ //const reddit_top_of_sub_data = function reddit_top_of_sub_data(sub, limet, time_filter){
+  
+
+
+ module.exports = {reddit_top_of_sub_data: (sub, limet, time_filter) => {
+  const axios = require('axios');
+  return new Promise(function(resolve, reject){
+    axios.get('https://www.reddit.com/r/'+ sub + '/.json?limit=' + limet + '&time_filter=' + time_filter)
+    .then(response => {
+      console.log(response.data)
+      var result = response.data;
+      resolve(result);
+
+    })
+      .catch(error => {
+        this.setState({});
+        console.log(error);
+        reject(error)
+    });
+
+  })
+
+
+
   
 }
+}
+//reddit_top_of_sub_data('ProRevenge', 10, "day")
 
-export default reddit_top_of_sub_data;
--- src/top_posts.js
+++ src/top_posts.js
@@ -1,4 +1,4 @@
-import reddit_top_of_sub_data from './api/reddit_top_of_sub_data';
+const api = require('./api/reddit_top_of_sub_data');
 const axios = require('axios');
 const React = require('react');
 
@@ -12,48 +12,45 @@ class TopPosts extends React.Component{
    
   }
   componentDidMount(){
-    // this.setState({
-    //   data: reddit_top_of_sub_data(this.props.sub, this.props.limet, this.props.time_filter)
 
-    // })
-  axios.get('https://www.reddit.com/r/'+ this.props.sub + '/.json?limit=' + this.props.limet + '&time_filter=' + this.props.time_filter)
-    .then(response => {
-      console.log(response.data)
+    const get_get_api =  async () => {
+      var result = await api.reddit_top_of_sub_data("ProRevenge", 10 ,"day")
+      console.log("result: ")
+      console.log(result)
       this.setState({
-        data: response.data[0]
+        data: await result
       })
-    })
-      .catch(error => {
-        this.setState({});
-        console.log(error);
-    });
-
-
-    // reddit_top_of_sub_data(this.props.sub, this.props.limet, this.props.time_filter)
+    }
+    get_get_api();
   }
 
   render(){
+    // const res_data = this.state.data;   
+    const res_data = "sdf"
+
+    console.log("idfk:")
+    window.idfk = res_data
 
 
-    console.log(this.state.data[0])
-
-    const res_data = this.state.data;    
-    // return(
-    //   <h1>dsf</h1>
-    // )
     return(
       <div>
-         {res_data.children.map(children => {
-          return(
-          <div>
-            <h1>{children.data.title}</h1>
-            <p>{children.data.selftext}</p>
-          </div>
-          )
-        })} 
-        <h1>test</h1>
+        <h1>{res_data}</h1>
+        <div>ts</div>
       </div>
     )
+    // return(
+    //   <div>
+    //      {res_data.children.map(children => {
+    //       return(
+    //       <div>
+    //         <h1>{children.data.title}</h1>
+    //         <p>{children.data.selftext}</p>
+    //       </div>
+    //       )
+    //     })} 
+    //     <h1>test</h1>
+    //   </div>
+    // )
   }
 }
 
